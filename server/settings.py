491e5bda2c2755a55277b3456b5df1c7fcc748aa
ilya sablin <ilya.sablin@engee.cloud>
2023-12-30 13:36:16 +0300

    init

--- server/settings.py
+++ server/settings.py
@@ -10,7 +10,7 @@ POSTGRES_USER = os.getenv('POSTGRES_USER', 'postgres')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'postgres')
 POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
 POSTGRES_PORT = os.getenv('POSTGRES_PORT', 5432)
-POSTGRES_DB = os.getenv('POSTGRES_DB', 'indigo')
+POSTGRES_DB = os.getenv('POSTGRES_DB', 'kindergarten')
 POSTGRES_CONNECT_STR = (
     f'postgresql+asyncpg://'
     f'{POSTGRES_USER}:'
